10a6b040a437feca837b03a53add712d6a52cdde
Darryl Karney <dkarney@outlook.com>
2022-04-19 19:34:07 -0500

    Get site to compile

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,10 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const Dashboard = ({}) => {
+  return <></>;
+};
+
+export default Dashboard;
