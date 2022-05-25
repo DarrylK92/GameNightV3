9f255641850c2ad16d593a2f0ec6579fb67feab1
Darryl Karney <dkarney@outlook.com>
2022-05-25 07:56:57 -0500

    Add reducers for events

---
+++ client/src/reducers/event.js
@@ -0,0 +1,49 @@
+import {
+    GET_EVENT,
+    EVENT_ERROR,
+    GET_EVENTS,
+    CLEAR_EVENT
+  } from '../actions/types';
+  
+  const initialState = {
+    events: [],
+    event: null,
+    loading: true,
+    error: {}
+  };
+  
+  function eventReducer(state = initialState, action) {
+    const { type, payload } = action;
+  
+    switch (type) {
+      case GET_EVENT:
+        return {
+          ...state,
+          event: payload,
+          loading: false
+        };
+      case GET_EVENTS:
+        return {
+          ...state,
+          events: payload,
+          loading: false
+        };
+      case EVENT_ERROR:
+        return {
+          ...state,
+          error: payload,
+          loading: false
+        };
+      case CLEAR_EVENT:
+        return {
+          ...state,
+          event: null,
+          loading: true
+        };
+      default:
+        return state;
+    }
+  }
+  
+  export default eventReducer;
+  
