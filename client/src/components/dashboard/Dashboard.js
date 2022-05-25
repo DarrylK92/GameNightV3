27a241e5034c54ff26a7b85fe4b6400aa3042cbc
Darryl Karney <dkarney@outlook.com>
2022-05-25 07:57:57 -0500

    Initial dashboard implementation

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,9 +2,45 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import DashboardActions from './DashboardActions';
+import { getOpenEvents } from '../../actions/event';
 
-const Dashboard = ({}) => {
-  return <></>;
+const Dashboard = ({ getOpenEvents, auth: { user }, event: { loading } }) => {
+  useEffect(() => {
+    getOpenEvents();
+
+    if (user === null) {
+      window.location.reload(false);
+    }
+  }, [getOpenEvents]);
+
+  return (
+    <section className="container">
+      <h1 className="large text-primary">Dashboard</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Welcome {user && user.name}
+      </p>
+      {loading === true && <Spinner />}
+      {loading === false ? (
+        <>
+          <DashboardActions />
+        </>
+      ) : (
+        <></>
+      )}
+    </section>
+  );
 };
 
-export default Dashboard;
+Dashboard.propTypes = {
+  getOpenEvents: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  event: state.event
+});
+
+export default connect(mapStateToProps, { getOpenEvents })(Dashboard);
