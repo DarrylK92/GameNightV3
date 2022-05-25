dd1cc465ba25b64e24f566b414c87ce7062b25de
Darryl Karney <dkarney@outlook.com>
2022-05-25 07:53:24 -0500

    Remove votedate

--- models/Vote.js
+++ models/Vote.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const VoteSchema = new Schema({
-  voteDate: Date,
-  gameId: { type: Schema.Types.ObjectId, ref: 'game' },
-  userId: { type: Schema.Types.ObjectId, ref: 'user' }
+  game: { type: Schema.Types.ObjectId, ref: 'game' },
+  user: { type: Schema.Types.ObjectId, ref: 'user' }
 });
 
 module.exports = mongoose.model('vote', VoteSchema);
