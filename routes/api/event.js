9a3c438f4969920d6f5ac2c5cf67998098abe892
Darryl Karney <dkarney@outlook.com>
2022-05-29 15:51:21 -0500

    Add routes for creating event and retrieving event by id

--- routes/api/event.js
+++ routes/api/event.js
@@ -31,7 +31,7 @@ router.get('/all/open/', async (req, res) => {
   try {
     // let events = await Event.find({ isOpen: true }).populate('createdBy', [
     //   'name'
-    // ]);
+    // ])
     // .populate({
     //     path: 'games',
     //     populate: {
@@ -60,5 +60,91 @@ router.get('/all/open/', async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @route    POST api/event
+// @desc     Create an event
+// @access   Private
+router.post('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+
+    if (!user) {
+      return res.status(404).json({ msg: 'User not found' });
+    }
+
+    let event;
+
+    if (req.body._id !== '') {
+      try {
+        event = await Event.findOneAndUpdate(
+          { _id: req.body._id },
+          {
+            $set: {
+              name: req.body.name,
+              autoCloseDate: req.body.date,
+              games: req.body.games
+            }
+          },
+          { new: true, upsert: true, setDefaultsOnInsert: true }
+        );
+      } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server Error');
+      }
+    } else {
+      event = new Event({
+        name: req.body.name,
+        autoCloseDate: req.body.date,
+        games: req.body.games
+      });
+
+      event = await event.save();
+    }
+
+    res.json(event);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route    GET api/event/:event_id
+// @desc     Get event by event ID
+// @access   Public
+router.get(
+  '/:event_id',
+  checkObjectId('event_id'),
+  async ({ params: { event_id } }, res) => {
+    try {
+      let event = await Event.findById(event_id).populate('createdBy', [
+        'name'
+      ])
+      .populate({
+          path: 'games',
+          populate: {
+              path: 'game',
+              model: 'game'
+          }
+      })
+      .populate({
+          path: 'votes',
+          populate: {
+              path: 'vote',
+              model: 'vote'
+          }
+      });
+
+      if (!event) {
+        return res.status(404).json({ msg: 'Event not found' });
+      }
+
+      res.json(event);
+    } catch (err) {
+      console.error(err.message);
+
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
