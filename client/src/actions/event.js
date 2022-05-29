9a8ed563acf6d9b6b53e9d698de617b3dd2cad6c
Darryl Karney <dkarney@outlook.com>
2022-05-29 15:51:51 -0500

    Add events for creating event and retrieving event by id

--- client/src/actions/event.js
+++ client/src/actions/event.js
@@ -2,23 +2,71 @@ import api from '../utils/api';
 import { setAlert } from './alert';
 
 import {
-    EVENT_CREATED,
-    EVENT_DELETED,
-    EVENT_UPDATED,
-    GET_EVENT,
-    GET_EVENTS,
-    EVENT_ERROR,
-    CLEAR_EVENT
-  } from './types';
+  EVENT_CREATED,
+  EVENT_DELETED,
+  EVENT_UPDATED,
+  GET_EVENT,
+  GET_EVENTS,
+  EVENT_ERROR,
+  CLEAR_EVENT
+} from './types';
 
 //Get all open events
 export const getOpenEvents = () => async (dispatch) => {
+  try {
+    const res = await api.get(`/event/all/open/`);
+
+    dispatch({
+      type: GET_EVENTS,
+      payload: res.data
+    });
+  } catch (err) {}
+};
+
+//Create or update event
+export const createEvent =
+  (formData, navigate, edit = false) =>
+  async (dispatch) => {
     try {
-      const res = await api.get(`/event/all/open/`);
-  
+      const res = await api.post('/event', formData);
+
       dispatch({
-        type: GET_EVENTS,
+        type: GET_EVENT,
         payload: res.data
       });
-    } catch (err) {}
+
+      dispatch(setAlert(edit ? 'Event Updated' : 'Event Created', 'success'));
+
+      if (!edit) {
+        navigate('/dashboard');
+      }
+    } catch (err) {
+      const errors = err.response.data.errors;
+
+      if (errors) {
+        errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
+      }
+
+      dispatch({
+        type: EVENT_ERROR,
+        payload: { msg: err.response.statusText, status: err.response.status }
+      });
+    }
   };
+
+//Get event
+export const getEvent = (id) => async (dispatch) => {
+  try {
+    const res = await api.get(`event/${id}`);
+
+    dispatch({
+      type: GET_EVENT,
+      payload: res.data
+    });
+  } catch (err) {
+    dispatch({
+      type: EVENT_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
