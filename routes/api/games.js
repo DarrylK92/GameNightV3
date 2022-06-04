585e14c8674be99f0ee20385c96a31a1f5d67d6a
Darryl Karney <dkarney@outlook.com>
2022-06-03 19:19:50 -0500

    Add game route file with route to retrieve all games

---
+++ routes/api/games.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const axios = require('axios');
+const config = require('config');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const checkObjectId = require('../../middleware/checkObjectId');
+
+const Game = require('../../models/Game');
+
+// @route    GET api/games/
+// @desc     Get all games
+// @access   Public
+router.get('/', async (req, res) => {
+  try {
+    let games = await Game.find().populate('ownerId', ['name']).sort('name');
+
+    res.json(games);
+  } catch (err) {
+    console.error(err.message);
+
+    res.status(500).send('Server Error');
+  }
+});
+
+module.exports = router;
